329a7810245228757ccaa2d99c1530180e539a12
michaelklos <michael.klos@gmail.com>
2022-03-20 16:26:10 -0400

    feat: division and multiplication features done

--- py/test_money.py
+++ py/test_money.py
@@ -1,19 +1,37 @@
+from curses import ALL_MOUSE_EVENTS
 import unittest
 
 
-class Dollar:
-    def __init__(self, amount) -> None:
+class Money:
+    def __init__(self, amount, currency) -> None:
         self.amount = amount
+        self.currency = currency
+
+    def __eq__(self, other):
+        return self.amount == other.amount and self.currency == other.currency
 
     def times(self, multiplier):
-        return Dollar(self.amount * multiplier)
+        return Money(self.amount * multiplier, self.currency)
+
+    def divide(self, divisor):
+        return Money(self.amount / divisor, self.currency)
 
 
 class TestMoney(unittest.TestCase):
-    def testMultiplication(self):
-        fiver = Dollar(5)
-        tenner = fiver.times(2)
-        self.assertEqual(10, tenner.amount)
+    def testMultiplicationInDollars(self):
+        fiveDollars = Money(5, "USD")
+        tenDollars = Money(10, "USD")
+        self.assertEqual(tenDollars, fiveDollars.times(2))
+
+    def testMultiplicationInEuros(self):
+        tenEuros = Money(10, "EUR")
+        twentyEuros = Money(20, "EUR")
+        self.assertEqual(twentyEuros, tenEuros.times(2))
+
+    def testDivisionInKoreanWon(self):
+        originalMoney = Money(4002, "KRW")
+        expectedMoneyAfterDivision = Money(1000.5, "KRW")
+        self.assertEqual(expectedMoneyAfterDivision, originalMoney.divide(4))
 
 
 if __name__ == "__main__":
