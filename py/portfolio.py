b955fcdd5ae45710dcb47ac086ccf164fda6b45c
michaelklos <michael.klos@gmail.com>
2022-03-21 00:14:41 -0400

    feat: conversion of Money from EUR to USD

--- py/portfolio.py
+++ py/portfolio.py
@@ -7,10 +7,19 @@ import operator
 class Portfolio:
     def __init__(self) -> None:
         self.moneys = []
+        self._eur_to_usd = 1.2
+
+    def __convert(self, aMoney, aCurrency):
+        if aMoney.currency == aCurrency:
+            return aMoney.amount
+        else:
+            return aMoney.amount * self._eur_to_usd
 
     def add(self, *moneys):
         self.moneys.extend(moneys)
 
     def evaluate(self, currency):
-        total = functools.reduce(operator.add, map(lambda m: m.amount, self.moneys), 0)
+        total = functools.reduce(
+            operator.add, map(lambda m: self.__convert(m, currency), self.moneys), 0
+        )
         return Money(amount=total, currency=currency)
